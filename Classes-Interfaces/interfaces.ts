2718edb6df6d2ef2ac8193bdc82da67558570f87
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-10-06 09:16:10 +0100

    TS Interfaces

---
+++ Classes-Interfaces/interfaces.ts
@@ -0,0 +1,21 @@
+interface IPerson {
+  readonly name? : string;
+  age: number;
+  greet(phrase: string): void;
+}
+
+class Person implements IPerson {
+  constructor(public name?: string, public age: number) {}
+
+  greet(phrase: string) {
+    console.log(phrase);
+  }
+}
+
+//Interfaces as Function types
+
+type addFn = (n1: number, n2: number) => number;
+
+interface AddFn {
+  (n1: number, n2: number): number;
+}
