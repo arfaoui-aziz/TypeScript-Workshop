563c014293c1c4d34f6a80acc492cf83d4999928
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-10-07 17:55:08 +0100

    TS generic Classes

--- Generics/app.ts
+++ Generics/app.ts
@@ -43,3 +43,45 @@ const mergedObj3 = mergeObjects2<{ name: string }, { age: number }>(
   { name: "aziz" },
   { age: 23 }
 );
+
+//Constraints
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// key of
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+extractAndConvert({ name: "Max" }, "name");
+
+//Generic Classes
+
+class DataStorage<T> {
+  private data: T[];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("test");
+
+const numberStorage = new DataStorage<number>();
+
+numberStorage.addItem(23);
