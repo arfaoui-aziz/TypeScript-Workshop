c8eb36199e46252be9284839cd51344417bd8a99
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-10-15 20:31:51 +0100

    Prjects Management App Selecting DOM Elements

--- ProjectManagementApp/dist/app.js
+++ ProjectManagementApp/dist/app.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("working");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.formElement = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+    }
+}
+const project = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- ProjectManagementApp/src/app.ts
+++ ProjectManagementApp/src/app.ts
@@ -1,2 +1,28 @@
 // Code goes here!
-console.log("working");
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+  }
+}
+
+const project = new ProjectInput();
